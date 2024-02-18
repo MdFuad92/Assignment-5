39ab822eab99c011f0258e4c4d0860421a5af95a
Spicy Gore <mdfuad98@gmail.com>
2024-02-18 20:42:19 +0600

    first commit

---
+++ jsfile/function.js
@@ -0,0 +1,10 @@
+function seatSelect(){
+    const selectSeat = document.getElementsByClassName('A1')
+    const newcolor = selectSeat.style.backgroundColor = 'green'
+  if(newcolor === 'click'){
+    
+  }
+      
+     
+    
+}
